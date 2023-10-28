5b039d313692ac78dd3c278112f21746dcb357a4
yasna2002 <yasnatalebi@gmail.com>
2023-10-28 18:08:13 +0330

    cleaning Dataset and dropping duplicates

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pandas as pd
+
 df = pd.read_csv("Flight_Data.csv")
-print(df)
+
+data2 = df.drop(columns='Airline')
+
+df2 = data2.drop_duplicates(keep='first')
+
+extracted_col = df["Airline"]
+df2.insert(0, 'Airline', extracted_col) # removing duplicates
+
+#print(df2)
+
