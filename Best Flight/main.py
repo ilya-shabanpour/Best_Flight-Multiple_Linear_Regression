d220fab5fb21939eb46ce55cd1a2ddaf5b976a9e
yasna2002 <yasnatalebi@gmail.com>
2023-11-07 20:26:43 +0330

    some bugs been handled

--- Best Flight/main.py
+++ Best Flight/main.py
@@ -141,11 +141,6 @@ def a_star_graph():
 # -----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    # df = pd.read_csv("Flight_Data.csv")
-    # data2 = df.drop(columns='Airline')
-    # df2 = data2.drop_duplicates(keep='first')
-    # extracted_col = df["Airline"]
-    # df2.insert(0, 'Airline', extracted_col)
 
     user_input = input("Please enter source and destination airport:")
     input_split = user_input.split(" - ")
