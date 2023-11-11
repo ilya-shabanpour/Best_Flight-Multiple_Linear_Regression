468428ffc03b4b3c10418523c789b0c8921134e0
Ilya Shabanpour Foladi <ilyashabanpour@gmail.com>
2023-11-11 15:42:00 +0330

    regression completed

--- Multiple Linear Regression/main.py
+++ Multiple Linear Regression/main.py
@@ -1,48 +1,42 @@
+import math
+import time
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 
-def cost(x_tr, y_tr, coefficient):
-    total_cost = 0
-    n = len(x_tr)
-    temp = np.dot(x_tr, coefficient) - y_tr
-    total_cost = (1 / n) * np.dot(np.transpose(temp), temp)
-    return total_cost
+def output_file(coefficient, exe_t, mse, rmse, mae, r2):
+    file = open("[8]-UIAI4021-PR1-Q2.txt", "w+")
+    file.write("Price: ")
+    file.write(str(round(coefficient[0], 2)) + " * [departure_time] + " + str(round(coefficient[1], 2))
+               + " * [stops] + " + str(round(coefficient[2], 2)) + " * [arrival_time] + "
+               + str(round(coefficient[3], 2)) + " * [class] + " + str(round(coefficient[4], 2))
+               + " * [duration] + " + str(round(coefficient[5], 2)) + " * [days_left] + "
+               + str(round(coefficient[6], 2)))
+    file.write("\nTraining Time: " + str(exe_t) + "s\n\n")
+    file.write("Logs:")
+    file.write("\nMSE: " + str(mse))
+    file.write("\nRMSE: " + str(rmse))
+    file.write("\nMAE: " + str(mae))
+    file.write("\nR2: " + str(r2))
+    file.close()
 
 
 def gradient_decent(x_tr, y_tr, coefficient, learning_rate, epoch):
     n = len(x_tr)
     for i in range(epoch):
-        # slopes = (2/n) * np.dot(np.transpose(np.dot(x_tr, coefficient) - y_tr), x_tr)
         predictions = np.dot(x_tr, coefficient)
         errors = predictions - y_tr
         gradient = (2/n) * x_tr.T.dot(errors)
         coefficient = coefficient - learning_rate * gradient
-        # print(cost(x_tr, y_tr, coefficient))
     return coefficient
 
 
 if __name__ == '__main__':
     df = pd.read_csv("Flight_Price_Dataset_Q2.csv")
-
-    # ohe = OneHotEncoder()
-    #
-    # feature_array = ohe.fit_transform(df[['stops', 'arrival_time', 'departure_time']]).toarray()
-    # labels = ["one", "two_or_more", "zero", "arrival_time_Afternoon", "arrival_time_Early_Morning",
-    #           "arrival_time_Evening", "arrival_time_Late_Night", "arrival_time_Morning", "arrival_time_Night",
-    #           "departure_time_Afternoon", "departure_time_Early_Morning", "departure_time_Evening",
-    #           "departure_time_Late_Night", "departure_time_Morning", "departure_time_Night"]
-    #
-    # dataframe = pd.DataFrame(feature_array, columns=labels)
-    # df = df.drop("departure_time", axis=1)
-    # df = df.drop("arrival_time", axis=1)
-    # df = df.drop("stops", axis=1)
-    # df = pd.concat([dataframe, df], axis=1)
 
     df["stops"] = df["stops"].map({"zero": 1, "one": 2, "two_or_more": 3})
 
@@ -54,18 +48,6 @@ if __name__ == '__main__':
 
     df['class'] = df['class'].map({'Economy': 1, 'Business': 10})
 
-    # label = ["one", "two_or_more", "zero", "arrival_time_Afternoon", "arrival_time_Early_Morning",
-    #          "arrival_time_Evening", "arrival_time_Late_Night", "arrival_time_Morning", "arrival_time_Night",
-    #          "departure_time_Afternoon",
-    #          "departure_time_Early_Morning", "departure_time_Evening", "departure_time_Late_Night",
-    #          "departure_time_Morning", "departure_time_Night", "class", "duration", "days_left", "price"]
-
-    # data = np.array(df)
-    # scaler = MinMaxScaler()
-    # normalized = scaler.fit_transform(data)
-    # df = pd.DataFrame(normalized, columns=label)
-
-    # x = df.drop("price", axis=1).values
     x = df.iloc[0:, 0:6]
     x = np.array(x)
     x = (x - x.mean()) / x.std()
@@ -74,16 +56,21 @@ if __name__ == '__main__':
     x = np.array(x)
     y = df["price"].values
 
-    # y = (y - y.mean()) / y.std()
-
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
     coefficients = np.zeros(7)
-    epochs = 2000
+    epochs = 1000
     L = 0.1
+
+    start = time.time()
     coefficients = gradient_decent(x_train, y_train, coefficients, L, epochs)
+    end = time.time()
+
+    exe_time = round(end - start, 1)
+
     y_pred = np.dot(x_test, coefficients)
     r2 = round(r2_score(y_test, y_pred), 2)
-    mse = mean_squared_error(y_test, y_pred)
-    print(coefficients)
-    print("MSE: ", str(mse))
-    print("R2: ", str(r2))
+    mse = round(mean_squared_error(y_test, y_pred))
+    rmse = round(math.sqrt(mse))
+    mae = round(mean_absolute_error(y_test, y_pred))
+
+    output_file(coefficients, exe_time, mse, rmse, mae, r2)
